d505a2dffa549a001ffe6fd769532ca6ce08ac24
soufiane saidi <61837299+soufianessaidimrc@users.noreply.github.com>
2021-01-21 15:16:02 +0100

    Update PersonController.java

--- src/main/java/com/dre/demo/api/PersonController.java
+++ src/main/java/com/dre/demo/api/PersonController.java
@@ -13,14 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
     @GetMapping()
     public String home() {
-                return "write down your name and last name in the path like this: THISPATH/spring";
+                return "write down your name and last name in the path like this: <b>THISPATH/spring</b>";
     }
 
     @GetMapping("/spring")
     public String test() {
-        return "En informatique, Spring est un framework open source pour construire et définir l'infrastructure d'une application Java3, dont il facilite le développement et les tests.\n"
-        		+"En 2004, Rod Johnson a écrit le livre Expert One-on-One J2EE Design and Development4 qui explique les raisons de la création de Spring.</b>\n"
-        		+ "<b>write down your name and last name in the path like this: THISPATH/welcometomypage/name/lastname";
+        return "En informatique, Spring est un framework open source pour construire et définir l'infrastructure d'une application Java3, dont il facilite le développement et les tests.\n<br/>"
+        		+"En 2004, Rod Johnson a écrit le livre Expert One-on-One J2EE Design and Development4 qui explique les raisons de la création de Spring.<br/>"
+        		+ "write down your name and last name in the path like this: <b>THISPATH/welcometomypage/name/lastname</b>";
     }
     @GetMapping(path = "/welcometomypage/{name}/{sname}")
     public String message(@PathVariable("name") String name,@PathVariable("sname") String sname) {
