22ee243c6bcdcf6a734300c9470f891e46294cb0
soufiane saidi <61837299+soufianessaidimrc@users.noreply.github.com>
2021-01-21 15:08:17 +0100

    Update PersonController.java

--- src/main/java/com/dre/demo/api/PersonController.java
+++ src/main/java/com/dre/demo/api/PersonController.java
@@ -18,7 +18,9 @@ public class PersonController {
 
     @GetMapping("/spring")
     public String test() {
-        return "Heloo World from Spring";
+        return "En informatique, Spring est un framework open source pour construire et définir l'infrastructure d'une application Java3, dont il facilite le développement et les tests."
+        		+"En 2004, Rod Johnson a écrit le livre Expert One-on-One J2EE Design and Development4 qui explique les raisons de la création de Spring.</b>"
+        		+ "<b>write down your name and last name in the path like this: PATH/welcometomypage/name/lastname";
     }
     @GetMapping(path = "/welcometomypage/{name}/{sname}")
     public String message(@PathVariable("name") String name,@PathVariable("sname") String sname) {
